43f162de649494f36bb32ee54de09e14fd732c36
SamueleAlpino <samuelealpino@gmail.com>
2018-05-27 15:48:46 +0200

    Added feature generate, drag and drop

---
+++ Agile_Test/Assets/PanelController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelController : MonoBehaviour 
+{
+    private GameObject toControl;
+
+    public void GeneratePrefab(GameObject toInstantiate)
+    {
+        toControl = Instantiate(toInstantiate);
+     //   toControl.transform.position = Camera.main.WorldToScreenPoint(Input.mousePosition);
+        toControl.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y,0f);
+        toControl.AddComponent<PanelController>();
+    }
+
+    public void ControlPosition()
+    {
+        this.transform.position = Input.mousePosition;
+    }
+}
